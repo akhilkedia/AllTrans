b108bb0fb073e50ce5f34d24dbcbcda4060f6945
Akhil Kedia <akhil.kedia@samsung.com>
2020-04-07 23:41:30 +0900

    API v3 works, need to update languages
    Signed-off-by: Akhil Kedia <akhil.kedia@samsung.com>

--- app/src/main/java/akhil/alltrans/GetTranslateToken.java
+++ app/src/main/java/akhil/alltrans/GetTranslateToken.java
@@ -23,6 +23,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -40,11 +44,13 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-class GetTranslateToken implements Callback {
+//class GetTranslateToken implements Callback {
+class GetTranslateToken {
     private static final Semaphore available = new Semaphore(1, true);
-    private static String userCredentials;
-    private static long lastExpireTime = 0;
+//    private static String userCredentials;
+//    private static long lastExpireTime = 0;
     private static OkHttpClient httpsClient;
+    private static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
     public GetTranslate getTranslate;
 
     private static Cache createHttpsClientCache() {
@@ -61,41 +67,42 @@ class GetTranslateToken implements Callback {
                     .cache(cache).build();
         }
         available.release();
-        if (PreferenceList.EnableYandex)
-            doInBackground();
-        else {
-            available.acquireUninterruptibly();
-            long time = System.currentTimeMillis();
-            if (time > lastExpireTime) {
-                utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Entering get new token for string : " + getTranslate.stringToBeTrans);
-                getNewToken();
-            } else {
-                available.release();
-                doInBackground();
-            }
-        }
+        doInBackground();
+//        if (PreferenceList.EnableYandex)
+//            doInBackground();
+//        else {
+//            available.acquireUninterruptibly();
+//            long time = System.currentTimeMillis();
+//            if (time > lastExpireTime) {
+//                utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Entering get new token for string : " + getTranslate.stringToBeTrans);
+//                getNewToken();
+//            } else {
+//                available.release();
+//                doInBackground();
+//            }
+//        }
     }
 
-    private void getNewToken() {
-        try {
-            MediaType mediaType = MediaType.parse("application/jwt");
-            RequestBody body = RequestBody.create(mediaType, "");
-
-            Request request = new Request.Builder()
-                    .url("https://api.cognitive.microsoft.com/sts/v1.0/issueToken")
-                    .post(body)
-                    .addHeader("Ocp-Apim-Subscription-Key", PreferenceList.SubscriptionKey)
-                    .addHeader("Content-Type", "application/json")
-                    .addHeader("Accept", "application/jwt")
-                    .cacheControl(CacheControl.FORCE_NETWORK)
-                    .build();
-
-            httpsClient.newCall(request).enqueue(this);
-
-        } catch (Exception e) {
-            Log.e("AllTrans", "AllTrans: Got error in getting new token as : " + Log.getStackTraceString(e));
-        }
-    }
+//    private void getNewToken() {
+//        try {
+//            MediaType mediaType = MediaType.parse("application/jwt");
+//            RequestBody body = RequestBody.create(mediaType, "");
+//
+//            Request request = new Request.Builder()
+//                    .url("https://api.cognitive.microsoft.com/sts/v1.0/issueToken")
+//                    .post(body)
+//                    .addHeader("Ocp-Apim-Subscription-Key", PreferenceList.SubscriptionKey)
+//                    .addHeader("Content-Type", "application/json")
+//                    .addHeader("Accept", "application/jwt")
+//                    .cacheControl(CacheControl.FORCE_NETWORK)
+//                    .build();
+//
+//            httpsClient.newCall(request).enqueue(this);
+//
+//        } catch (Exception e) {
+//            Log.e("AllTrans", "AllTrans: Got error in getting new token as : " + Log.getStackTraceString(e));
+//        }
+//    }
 
     private void doInBackground() {
         try {
@@ -114,20 +121,24 @@ class GetTranslateToken implements Callback {
                 utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Enqueuing Request for new translation for : " + getTranslate.stringToBeTrans);
                 httpsClient.newCall(request).enqueue(getTranslate);
             } else {
-                String baseURL = "https://api.microsofttranslator.com/v2/Http.svc/Translate?text=";
+                String baseURL = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0";
                 String languageURL = "&from=" + PreferenceList.TranslateFromLanguage + "&to=" + PreferenceList.TranslateToLanguage;
-                String fullURL = baseURL + URLEncoder.encode(getTranslate.stringToBeTrans, "UTF-8") + languageURL;
+                String fullURL = baseURL + languageURL;
+
+                String requestBodyJson = new JSONArray().put(new JSONObject().put("Text", getTranslate.stringToBeTrans)).toString();
+                RequestBody body = RequestBody.create(requestBodyJson, JSON_MEDIA_TYPE);
 
                 Request request = new Request.Builder()
                         .url(fullURL)
-                        .get()
-                        .addHeader("authorization", "Bearer " + userCredentials)
+                        .method("POST", body)
+                        .addHeader("Ocp-Apim-Subscription-Key", PreferenceList.SubscriptionKey)
+                        .addHeader("Content-Type", "application/json; charset=UTF-8")
                         .build();
 
                 utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Enqueuing Request for new translation for : " + getTranslate.stringToBeTrans);
                 httpsClient.newCall(request).enqueue(getTranslate);
             }
-        } catch (java.io.IOException e) {
+        } catch (IOException | JSONException e) {
             Log.e("AllTrans", "AllTrans: Got error in getting translation as : " + Log.getStackTraceString(e));
             if (getTranslate.canCallOriginal) {
                 new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
@@ -140,27 +151,27 @@ class GetTranslateToken implements Callback {
         }
     }
 
-    public void onResponse(Call call, Response response) {
-        try {
-            String result = response.body().string();
-            response.body().close();
-            utils.debugLog("Got request result as : " + result);
-            userCredentials = result;
-            utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Set User Credentials as : " + userCredentials);
-            lastExpireTime = System.currentTimeMillis() + 550000;
-        } catch (java.io.IOException e) {
-            Log.e("AllTrans", "AllTrans: Got error in getting token as : " + Log.getStackTraceString(e));
-        } finally {
-            available.release();
-            doInBackground();
-        }
-    }
-
-    @Override
-    public void onFailure(Call call, IOException e) {
-        Log.e("AllTrans", "AllTrans: Got error in getting token as : " + Log.getStackTraceString(e));
-        available.release();
-        doInBackground();
-    }
+//    public void onResponse(Call call, Response response) {
+//        try {
+//            String result = response.body().string();
+//            response.body().close();
+//            utils.debugLog("Got request result as : " + result);
+//            userCredentials = result;
+//            utils.debugLog("In Thread " + Thread.currentThread().getId() + "  Set User Credentials as : " + userCredentials);
+//            lastExpireTime = System.currentTimeMillis() + 550000;
+//        } catch (java.io.IOException e) {
+//            Log.e("AllTrans", "AllTrans: Got error in getting token as : " + Log.getStackTraceString(e));
+//        } finally {
+//            available.release();
+//            doInBackground();
+//        }
+//    }
+//
+//    @Override
+//    public void onFailure(Call call, IOException e) {
+//        Log.e("AllTrans", "AllTrans: Got error in getting token as : " + Log.getStackTraceString(e));
+//        available.release();
+//        doInBackground();
+//    }
 
 }
