48233a3040aa47dcfc910c23c1a4f144c8466175
Akhil Kedia <akhilkedia94@gmail.com>
2021-12-29 14:53:54 +0900

    updated dependencies
    Signed-off-by: Akhil Kedia <akhilkedia94@gmail.com>

--- app/src/main/java/akhil/alltrans/MainActivity.java
+++ app/src/main/java/akhil/alltrans/MainActivity.java
@@ -26,12 +26,8 @@ import android.util.Log;
 import android.view.View;
 
 import com.codemybrainsout.ratingdialog.RatingDialog;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 import com.google.gson.Gson;
 
 import org.jetbrains.annotations.NotNull;
